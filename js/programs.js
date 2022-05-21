34444e48b2c31a9188899f2bba5d74079419be1c
SirriCelles <sirricelles@gmail.com>
2022-05-21 09:14:27 +0100

    add js linters remove html-css linters from gitignore

--- js/programs.js
+++ js/programs.js
@@ -3,35 +3,35 @@ const programs = [
     name: 'Talks',
     imageUrl: './assets/img/speaker-icon.png',
     imageAlt: 'Speak_icon',
-    text: "Listen to speakers who are Africa's Top Women in Technology from various countries."
+    text: "Listen to speakers who are Africa's Top Women in Technology from various countries.",
   },
   {
     name: 'Labs',
     imageUrl: './assets/img/board-icon.png',
     imageAlt: 'Board_icon',
-    text: 'Appreciate hands on implimentaion of recent technologies like VR, Metaverse, AI, IOT and ML'
+    text: 'Appreciate hands on implimentaion of recent technologies like VR, Metaverse, AI, IOT and ML',
   },
 
   {
     name: 'Panel Time',
     imageUrl: './assets/img/message-icon.png',
     imageAlt: 'Board_icon',
-    text: 'Open discussions about topics that matter most to Women through their development journey. Some myths, difficulties and how to overcome them'
+    text: 'Open discussions about topics that matter most to Women through their development journey. Some myths, difficulties and how to overcome them',
   },
 
   {
     name: 'Starters pitch',
     imageUrl: './assets/img/write-icon.png',
     imageAlt: 'Board_icon',
-    text: 'Participants get 10mins to pitch projects and share their brilliant startup ideas. Winners of the starter pitch get award a startup package at the end of the conference.'
+    text: 'Participants get 10mins to pitch projects and share their brilliant startup ideas. Winners of the starter pitch get award a startup package at the end of the conference.',
   },
 
   {
     name: 'Network',
     imageUrl: './assets/img/setting-icon.png',
     imageAlt: 'Board_icon',
-    text: 'Get opportunities to network with other developers from around the globe.'
-  }
+    text: 'Get opportunities to network with other developers from around the globe.',
+  },
 ];
 
 const speakers = [
@@ -40,54 +40,54 @@ const speakers = [
     imageUser: './assets/img/Rebecca-Enonchong-780x780.jpg',
     header: 'Rebecca-Enonchong',
     qoute: 'Founder and Chief Executive Officer of AppsTech, a leading global provider of enterprise application solutions.',
-    text: 'The main obstacle to the digital revolution in Africa is the reluctance of political leaders'
+    text: 'The main obstacle to the digital revolution in Africa is the reluctance of political leaders',
   },
   {
     imageIcon: './assets/img/chess.jpg',
     imageUser: './assets/img/estelle.jpeg',
     header: 'Estelle Yomba',
     qoute: 'Social Entrepreneur with an unquenchable passion for quality Education.',
-    text: 'African Top entrepreneurs have the responsibilty to transfer professional know how by identifying, showcasing and moulding young African innovators ready to break records in transforming our rich continent'
+    text: 'African Top entrepreneurs have the responsibilty to transfer professional know how by identifying, showcasing and moulding young African innovators ready to break records in transforming our rich continent',
   },
   {
     imageIcon: './assets/img/chess.jpg',
     imageUser: './assets/img/Faith.webp',
     header: 'Mpara Faith',
     qoute: 'Mpara Faith is the co-founder and Operations manager of New Generation Technologies (NGT), ',
-    text: 'I am a Product Manager that enjoys empowering students with delightful and consistent learning experiences.'
+    text: 'I am a Product Manager that enjoys empowering students with delightful and consistent learning experiences.',
   },
   {
     imageIcon: './assets/img/chess.jpg',
     imageUser: './assets/img/jihu.jpeg',
     header: 'Jihan Abass',
     qoute: 'Founder and CEO of Lami,  an insurance as a service platform that aims to increase insurance penetration across Africa.',
-    text: 'For us it is really exciting to be able to get insurance companies to start looking at things in a different way, and also to provide [end customers] with products without any friction whatsoever in the process'
+    text: 'For us it is really exciting to be able to get insurance companies to start looking at things in a different way, and also to provide [end customers] with products without any friction whatsoever in the process',
   },
   {
     imageIcon: './assets/img/chess.jpg',
     imageUser: './assets/img/mihan.jpeg',
     header: 'Miishe Addy',
     qoute: ' co-founder and CEO of Jetstream Africa, a technology-enabled logistics company focusing on cross-border trade in Ghana.',
-    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eum laboriosam exercitationem dolorem cupiditate voluptate'
+    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eum laboriosam exercitationem dolorem cupiditate voluptate',
   },
   {
     imageIcon: './assets/img/chess.jpg',
     imageUser: './assets/img/user.jpg',
     header: 'Sirri Celles',
     qoute: 'Aspiring Fullstack developer.',
-    text: 'The purpose of our lives is to be happy. As developers apprecaite every step of the journey because you make lot of people happy.'
-  }
+    text: 'The purpose of our lives is to be happy. As developers apprecaite every step of the journey because you make lot of people happy.',
+  },
 ];
 
-let programWrapper = document.querySelector('.items-wrapper');
+const programWrapper = document.querySelector('.items-wrapper');
 
-let speakerWrapper = document.querySelector('.speakers-wrapper');
+const speakerWrapper = document.querySelector('.speakers-wrapper');
 let count = speakers.length;
 
 window.onload = (event) => {
-  programs.forEach(program => {
-    let item = document.createElement('div');
-    item.className = "program__items";
+  programs.forEach((program) => {
+    const item = document.createElement('div');
+    item.className = 'program__items';
     const programItem = `<div class="program__item-icon">
       <img src="${program.imageUrl}" alt="${program.imageAlt}">
     </div>
@@ -96,19 +96,19 @@ window.onload = (event) => {
     </div>
     <div class="program__item-text">
     ${program.text}
-    </div>`
+    </div>`;
 
     item.innerHTML = programItem;
-  programWrapper.appendChild(item);
+    programWrapper.appendChild(item);
   });
 
-  if(screen.width < 768) {
+  if (screen.width < 768) {
     count = 2;
   }
 
-  for(let i=0; i < count; i++) {
-    let item = document.createElement('div');
-    item.className = "speaker__wrapper";
+  for (let i = 0; i < count; i++) {
+    const item = document.createElement('div');
+    item.className = 'speaker__wrapper';
     const newSpeaker = `<div class="speaker__image">
     <img src="${speakers[i].imageIcon}" alt="Chess icon" class="speaker__image-icon">
       <img src="${speakers[i].imageUser}" alt="speaker" class="speaker__image-user">
@@ -127,5 +127,4 @@ window.onload = (event) => {
     item.innerHTML = newSpeaker;
     speakerWrapper.appendChild(item);
   }
-}
-
+};
--- js/script.js
+++ js/script.js
@@ -1,38 +1,36 @@
-'use strict';
-
 let toggleMenu;
 window.onload = (event) => {
   toggleMenu = false;
 };
 
-let landingPage = document.querySelector('#landingPage');
-let logoSmall = document.querySelector('.nav__logo-small');
+const landingPage = document.querySelector('#landingPage');
+const logoSmall = document.querySelector('.nav__logo-small');
 // close icon
-let closeiconWrapper = document.querySelector('.closeicon-wrapper');
-let menuCloseIcon = document.querySelector('.nav__closeicon');
-let menuIcon = document.querySelector('.nav__menu-icon');
-let backGround = document.querySelector('.show-background');
-let navMenu = document.querySelector('.nav__menu');
-let navList = document.querySelector('.nav__list');
-let menuLinks = document.querySelectorAll('.nav__list-link');
+const closeiconWrapper = document.querySelector('.closeicon-wrapper');
+const menuCloseIcon = document.querySelector('.nav__closeicon');
+const menuIcon = document.querySelector('.nav__menu-icon');
+const backGround = document.querySelector('.show-background');
+const navMenu = document.querySelector('.nav__menu');
+const navList = document.querySelector('.nav__list');
+const menuLinks = document.querySelectorAll('.nav__list-link');
 
-menuIcon.addEventListener('click', function() {
-  if(!toggleMenu) {
+menuIcon.addEventListener('click', () => {
+  if (!toggleMenu) {
     onOpenMenu();
     toggleMenu = true;
   }
 });
 
-menuCloseIcon.addEventListener('click', function() {
-  if(toggleMenu) {
+menuCloseIcon.addEventListener('click', () => {
+  if (toggleMenu) {
     onCloseMenu();
     toggleMenu = false;
   }
 });
 
-menuLinks.forEach(menuLink => {
-  menuLink.addEventListener('click', function() {
-    if(toggleMenu) {
+menuLinks.forEach((menuLink) => {
+  menuLink.addEventListener('click', () => {
+    if (toggleMenu) {
       onCloseMenu();
       toggleMenu = false;
     }
@@ -40,18 +38,18 @@ menuLinks.forEach(menuLink => {
 });
 
 // open menu
-let onOpenMenu = function() {
+let onOpenMenu = function () {
   backGround.style.display = 'block';
   closeiconWrapper.style.display = 'block';
   landingPage.classList.add('padding-0');
   navMenu.classList.add('nav__menu-small');
   navList.classList.add('nav__list-small');
-}
+};
 
-let onCloseMenu = function() {
+let onCloseMenu = function () {
   backGround.style.display = 'none';
   closeiconWrapper.style.display = 'none';
   landingPage.classList.remove('padding-0');
   navMenu.classList.remove('nav__menu-small');
   navList.classList.remove('nav__list-small');
-}
+};
